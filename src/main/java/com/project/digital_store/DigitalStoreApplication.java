b295d5e4e57d2a92e55d83ea22756c4e226de7a7
REINHARDT <s>
2020-07-09 11:26:25 +0800

    2020-07-09

---
+++ src/main/java/com/project/digital_store/DigitalStoreApplication.java
@@ -0,0 +1,29 @@
+package com.project.digital_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class DigitalStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalStoreApplication.class, args);
+    }
+
+    //配置允许跨域访问
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
+
+    }
+}
